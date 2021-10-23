f5b1c6a5ee18814df54a9d57ceb1bbe7b61bc0fe
Lucas Biffi <depressedps@gmail.com>
2021-10-23 01:57:02 -0300

    Adicionei a primeira versão do jogo_texto.py

---
+++ jogo_texto.py
@@ -0,0 +1,5 @@
+print("Bom dia, paciente.\n \nVocê passou...\n \nmil \n \nquinhentos \n \ne noventa e um \n \ndias desacordado.\n")
+
+nome = input("Não encontrei seus arquivos em meus servidores. Como você se chama? ")
+
+input("\nO seu nome é {}. Você está certo disso? (SIM ou NÃO)" .format(nome)) # Descobrir como limitar a escolha para apenas SIM ou NÃO
