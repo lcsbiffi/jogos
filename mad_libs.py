fe98c16d98bf68708f1cfbf7a13b6614c791324c
Lucas Biffi <depressedps@gmail.com>
2021-10-24 15:00:29 -0300

    Adicionei o primeiro arquivo de mad_libs.py. Preciso revisitar pra ver o que posso melhorar ou incrementar.

---
+++ mad_libs.py
@@ -0,0 +1,39 @@
+print("Bem-vindx ao jogo de Mad Libs do Lucas :) \n")
+
+print("Primeiro, me ajuda a escolher algumas palavras\n")
+
+adjetivo_um = input("Escolha um adjetivo: ")
+
+adjetivo_dois = input("Escolha outro adjetivo: ")
+
+ave = input("Agora, me ajude a escolher um tipo de ave: ")
+
+comodo = input("Escolha um cômodo da casa: ")
+
+verbo_passado = input("Um verbo no passado: ")
+
+verbo = input("Um verbo no infinitivo: ")
+
+nome = input("Digite o nome de um parente: ")
+
+substantivo_plural = input("Digite um substantivo no plural: ")
+
+liquido = input ("Escolha um líquido: ")
+
+verbo_um = input ("Digite outro verbo no presente: ")
+
+verbo_gerundio = input("Um verbo no gerúndio: ")
+
+substantivo_plural_um = input("Digite um novo substantivo no plural: ")
+
+verbo_gerundio_um = input("Outro verbo no gerúndio: ")
+
+substantivo_um = input("Por último, um substantivo: ")
+
+print("Aguarde enquanto preparamos o Mad Libs...\n \n")
+
+print("...\n \n")
+
+print("...\n \n")
+
+print("Era um dia frio e {} de Novembro. Eu acordei com o {} cheiro de {} assando no(a) {} lá embaixo. Eu {} escada abaixo para ver se podia ajudar a {} o jantar. Minha mãe disse 'Veja se {} precisa de {} frescos(as).' Então eu carreguei uma bandeja com copos cheios de {} para a sala de {}. Quando cheguei lá, não conseguia acreditar no que estava {}! Havia vários(as) {} {} em cima do(a) {}." .format(adjetivo_um, adjetivo_dois, ave, comodo, verbo_passado, verbo, nome, substantivo_plural, liquido, verbo_um, verbo_gerundio, substantivo_plural_um, verbo_gerundio_um, substantivo_um))
