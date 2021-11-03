fa665626abff2fdb86d7fcaef16183851ecb7d98
Lucas Biffi <depressedps@gmail.com>
2021-11-03 20:30:44 -0300

    Jogo da forca na versão que mostra a posição da letra encontrada

--- forca.py
+++ forca.py
@@ -10,6 +10,14 @@ def jogar():
     enforcou = False
 
     while(not acertou and not enforcou):
+        chute = input("Qual a letra? ")
+        chute = chute.strip() 
+
+        index = 0 # Serve para guardar a posição da letra encontrada
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format (letra, index))
+            index = index + 1    
         print("Jogando...")
 
     print("Fim de jogo")
